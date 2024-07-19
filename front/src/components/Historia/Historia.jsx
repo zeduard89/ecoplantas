d5ea59b3aa8b528ef937d7b137f3cd0ff573778b
Guillermo <zeduard89>
2024-07-19 00:33:35 -0300

    historia feat time line

--- front/src/components/Historia/Historia.jsx
+++ front/src/components/Historia/Historia.jsx
@@ -26,14 +26,18 @@ const Historia = () => {
       </h1>
 
       <div className="grid grid-cols-2 text-center border-t-2">
-        <div className="col-span-1 border-r-2 p-10 flex flex-col">
+        
+        <div className="col-span-1 border-r-4 p-10 flex flex-col relative">
+          <h1 className='content-center p-5 border-b-2 mb-9 w-[30%] sm:w-[36%] xl:w-[40%] absolute right-0	' ></h1>
           <h1 className='content-center text-3xl p-5'>2012</h1>
           <Card image={banner1} />
           <h3 className='content-center'>
             En 2012, Eco Plantas comenzó su viaje con una pequeña colección de macetas y plantas. Nuestros primeros eventos incluyeron bodas y eventos locales, donde rápidamente ganamos una reputación por nuestra atención al detalle y pasión por las plantas.
           </h3>
         </div>
-        <div className="col-span-1 p-10 pt-[100%] flex flex-col">
+
+        <div className="col-span-1 p-10 pt-[100%] flex flex-col relative">
+          <h1 className='content-center p-5 border-b-2 mb-9  absolute -left-1 w-[35%] sm:w-[40%]	' ></h1>
           <h1 className='content-center text-3xl p-5'>2013</h1>
           <Card image={banner1} />
           <h3 className='content-center'>
@@ -41,14 +45,17 @@ const Historia = () => {
           </h3>
         </div>
 
-        <div className="col-span-1 border-r-2 p-10 flex flex-col">
+        <div className="col-span-1 border-r-4 p-10 flex flex-col relative">
+          <h1 className='content-center p-5 border-b-2 mb-9 w-[30%] sm:w-[36%] xl:w-[40%] absolute right-0	' ></h1>
           <h1 className='content-center text-3xl p-5'>2014</h1>
           <Card image={banner1} />
           <h3 className='content-center'>
             Durante 2014, lanzamos una nueva línea de plantas exóticas y macetas personalizadas. Nuestro compromiso con la sostenibilidad también se fortaleció, comenzando a ofrecer opciones de reciclaje y compostaje para nuestras decoraciones.
           </h3>
         </div>
-        <div className="col-span-1 p-10 pt-[100%] flex flex-col">
+
+        <div className="col-span-1 p-10 pt-[100%] flex flex-col relative">
+          <h1 className='content-center p-5 border-b-2 mb-9  absolute -left-1 w-[35%] sm:w-[40%]	' ></h1>
           <h1 className='content-center text-3xl p-5'>2015</h1>
           <Card image={banner1} />
           <h3 className='pb-16 content-center'>
