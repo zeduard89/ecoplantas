faa0322d5c5cf4025347b584d6af9c604cc32d35
Guillermo <zeduard89>
2024-07-26 16:05:37 -0300

    cards funcionts

--- front/src/components/Card/CardMacetas.jsx
+++ front/src/components/Card/CardMacetas.jsx
@@ -12,6 +12,8 @@ const CardMacetas = ({id, title, content,imgUrl }) => {
     <div className='relative text-center rounded-md p-7 h-[25rem] w-[15rem] md:w-[18rem] lg:w-[18rem]'>
         <div className='absolute top-10 right-10 bg-green-700/30 border-2 border-gray-500 w-7 h-7 rounded'></div>
         <img src={imgUrl} alt='Imagen de la tarjeta' className='rounded-t-md w-full h-[65%] object-cover' />
+        <button className='absolute bottom-[7.8rem] left-9 w-6 h-6 rounded text-4xl'>+</button>
+        <button className='absolute bottom-[7.8rem] right-9 w-6 h-6 rounded text-4xl'>-</button>
         <h1 className='h-[2.5rem] text-white place-content-center bg-green-700 text-lg font-bold '>{title}</h1>
         {/* H3: Limited to 142 chars */}
         <div className=' bg-green-700/20 rounded-b-md text-[0.79rem] md:text-sm lg:text-sm overflow-hidden text-ellipsis text-left p-2'>
