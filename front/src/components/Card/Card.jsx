a6928c409fadcf60cf42ab0a4ffda52850ac1c93
Guillermo <zeduard89>
2024-07-26 13:37:52 -0300

    responsive historia

--- front/src/components/Card/Card.jsx
+++ front/src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ const Card = ( {image, shadowOn} ) => {
   return (
     <div className=' rounded-md ml-5 '>
       <div className='mb-4 '>
-        <img src={image} alt="Imagen" className={`rounded-md h-[20rem] w-[20rem] ${shadowOn ? 'shadow-card-history' : 'shadow-card-landing'}`} />
+        <img src={image} alt="Imagen" className={`rounded-md md:h-[20rem] md:w-[20rem] hidden md:block ${shadowOn ? 'shadow-card-history' : 'shadow-card-landing'}`} />
         
       </div>
     </div>
