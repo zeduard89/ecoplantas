9f1dad8825ba750f67d75aaf8885be301f05d7a1
Guillermo <zeduard89>
2024-07-27 02:23:53 -0300

    feat catalogo

--- front/src/components/Card/CardMacetas.jsx
+++ front/src/components/Card/CardMacetas.jsx
@@ -1,8 +1,23 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement, reset } from '../../redux/prosupuestoSlice';
 
 const CardMacetas = ({id, title, content,imgUrl }) => {
         
+    const dispatch = useDispatch();
+    const formattedTitle = title.toLowerCase().trim().replace(/\.$/, '').replace(/ /g, "");
+    const count = useSelector((state) => state.presupuesto[formattedTitle]);
+    // Actualiza el componente o realiza una acción cuando 'count' cambia
+
+    
+    const handleIncrement = () => {
+        dispatch(increment({ formattedTitle }));
+    };
+
+    const handleDecrement = () => {
+        dispatch(decrement({ formattedTitle }));
+    };
+
     if (!imgUrl) {
         console.error('Image is undefined or null');
         return null;
@@ -10,10 +25,24 @@ const CardMacetas = ({id, title, content,imgUrl }) => {
 
     return (
     <div className='relative text-center rounded-md p-7 h-[25rem] w-[15rem] md:w-[18rem] lg:w-[18rem]'>
-        <div className='absolute top-10 right-10 bg-green-700/30 border-2 border-gray-500 w-7 h-7 rounded'></div>
+        <div className='absolute top-10 right-10 font-bold bg-green-700/30 border-2 border-gray-500 w-7 h-7 rounded flex items-center justify-center'>
+            {count}
+        </div>
         <img src={imgUrl} alt='Imagen de la tarjeta' className='rounded-t-md w-full h-[65%] object-cover' />
-        <button className='absolute bottom-[7.8rem] left-9 w-6 h-6 rounded text-4xl'>+</button>
-        <button className='absolute bottom-[7.8rem] right-9 w-6 h-6 rounded text-4xl'>-</button>
+        <button 
+        className='absolute bottom-[7.7rem] left-9 w-6 h-6 rounded text-4xl  text-white'
+        onClick={handleIncrement}
+        aria-label='Increment'
+      >
+        +
+      </button>
+      <button 
+        className='absolute bottom-[7.8rem] right-9 w-6 h-6 rounded text-4xl  text-white'
+        onClick={handleDecrement}
+        aria-label='Decrement'
+      >
+        -
+      </button>
         <h1 className='h-[2.5rem] text-white place-content-center bg-green-700 text-lg font-bold '>{title}</h1>
         {/* H3: Limited to 142 chars */}
         <div className=' bg-green-700/20 rounded-b-md text-[0.79rem] md:text-sm lg:text-sm overflow-hidden text-ellipsis text-left p-2'>
